2a9c4f9a6bc9f6cf6ab2b71c86be956c46574a1a
debsourav33 <debsourav33@gmail.com>
2023-10-20 20:46:42 -0700

    added max contiguous chain function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
+#length is the minimum length of seq1 or seq2 after shifting either of them (and inserting "-" in the shifted cells)
 def find_match(seq1,seq2,length):
-    #this function is called with same lengthed sequence
+    
     match = 0
     for i in range(0,length):
         if seq1[i] == seq2[i]:
@@ -8,27 +9,53 @@ def find_match(seq1,seq2,length):
 
     return match
 
-    limit = min(len(seq1),len(seq2))
+#length is the minimum length of seq1 or seq2 after shifting either of them (and inserting "-" in the shifted cells)
+def find_chain(seq1,seq2,length):
+    
+    longest_chain = 0
+    match = 0
+    for i in range(0,length):
+        if seq1[i] == seq2[i]:
+            match += 1
+            longest_chain = max(longest_chain, match)
+        else:
+            match = 0
 
-def find_match_by_shifting_seq1(seq1, seq2, max_shift):
-    max_match = 0
+    return longest_chain
+
+def find_match_by_shifting_seq1(seq1, seq2, max_shift, chain = False):
+    max_score = 0
     for shift in range(0,max_shift+1):
-        shifted_seq1 = " " * shift + seq1
+        shifted_seq1 = '-' * shift + seq1
 
         smaller_length = min(len(shifted_seq1), len(seq2))
-        match = find_match(shifted_seq1,seq2,smaller_length)
+        
+        #chain or subsequence?
+        if chain:
+            score = find_chain(shifted_seq1,seq2,smaller_length)    
+        else:
+            score = find_match(shifted_seq1,seq2,smaller_length)
+        max_score = max(max_score, score)
 
-        print(f'For shift {shift}, match found: {match}')
+        print(f'For shift {shift}, score: {score}')
+    return max_score
 
-def find_match_by_shifting_seq2(seq1, seq2, max_shift):
-    max_match = 0
+def find_match_by_shifting_seq2(seq1, seq2, max_shift, chain = False):
+    max_score = 0
     for shift in range(0,max_shift+1):
-        shifted_seq2 = " " * shift + seq2
+        shifted_seq2 = "-" * shift + seq2
 
         smaller_length = min(len(shifted_seq2), len(seq1))
-        match = find_match(shifted_seq2,seq1,smaller_length)
+        
+        #chain or subsequence?
+        if chain:
+            score = find_chain(shifted_seq2,seq1,smaller_length)
+        else:
+            score = find_match(shifted_seq2,seq1,smaller_length)
+        max_score = max(max_score, score)
 
-        print(f'For shift {shift}, match found: {match}')
+        print(f'For shift {shift}, score: {score}')
+    return max_score
 
 def main():
 
@@ -50,11 +77,14 @@ def main():
             lines = file.readlines()
             sequence_one = lines[0][:-1] #trim the new line
             sequence_two = lines[1][:-1] #trim the new line
+            
+            print("The pair of sequence:")
             print(sequence_one)
             print(sequence_two)
+            print("---------------------")
 
-            #find_match_by_shifting_seq1(sequence_one,sequence_two,max_shift)
-            find_match_by_shifting_seq2(sequence_one,sequence_two,max_shift)
+            find_match_by_shifting_seq1(sequence_one,sequence_two,max_shift,chain=True)
+            #find_match_by_shifting_seq2(sequence_one,sequence_two,max_shift,chain=True)
     except FileNotFoundError:
         print(f"Please make sure the file {filename} exists in the same directory")
 
